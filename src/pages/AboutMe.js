550b3263c64e3204cbde457f80576e22416298a7
Tarek <108202992+tarek339@users.noreply.github.com>
2023-01-27 19:06:17 +0100

    Update AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,13 +6,13 @@ export default function AboutMe() {
       <h2>Webentwickler aus Hamburg</h2>
         <p>
             Ich bin ein Webentwickler mit einer Vorliebe für modernste Webtechnologien, 
-            der React, Node und MongoDB verwendet, um leistungsstarke Webanwendungen zu entwickeln. 
+            der React, Node.js und MongoDB verwendet, um leistungsstarke Webanwendungen zu entwickeln. 
             Ich bin in der Lage, jede Art von Web-Applikation z. Bsp. Onlineshops zu entwickeln, 
             einschließlich des Frontends und des Backends.
         </p>
         <p>
-            Zur Zeit lebe ich in Hamburg und habe eine Ausbildung zum Webentwickler erfolgreich absolviert 
-            und meinen Abschluss und mein Zertifikat erhalten.
+            Zur Zeit lebe ich in Hamburg und habe eine Ausbildung bei der ILS als Webentwickler erfolgreich absolviert.
+            Dadurch erhielt ich einen Abschluss und ein Zertifikat
         </p>
     </div>
   )
